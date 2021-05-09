d5324c37afb1bc8b8da212952b57c9183af8703f
Marcin Wosinek <marcin@wosinek.eu>
2021-05-09 11:55:37 +0100

    add pdf button & PoC the callback

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 const element = document.getElementById("core");
 
 element.innerHTML = "✓ Core module is loaded correclty";
+
+const pdfButton = document.getElementById("pdf-button");
+
+pdfButton.addEventListener("click", () => {
+  console.log("test");
+});
