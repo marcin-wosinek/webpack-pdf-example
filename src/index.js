9f68658572155fbe299b4241717df199c6cf65b4
Marcin Wosinek <marcin@wosinek.eu>
2021-05-09 13:17:23 +0100

    improve example UI

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ element.innerHTML = "✓ Core module is loaded correclty";
 const pdfButton = document.getElementById("pdf-button");
 
 import(/* webpackChunkName: "pdf-lib" */ "pdf-lib").then(({ PDFDocument }) => {
+  document.getElementById("pdf-lib").innerHTML =
+    "✓ pdf-lib is loaded correclty";
+
   pdfButton.addEventListener("click", async () => {
     const pdfDoc = await PDFDocument.create();
     const page = pdfDoc.addPage([350, 400]);
