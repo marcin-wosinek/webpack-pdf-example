e78ba097c230d3db7b777fd4de391fed94fbd125
Marcin Wosinek <marcin@wosinek.eu>
2021-05-09 13:15:14 +0100

    lazy load

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import { PDFDocument } from "pdf-lib";
-
 const element = document.getElementById("core");
 
 element.innerHTML = "✓ Core module is loaded correclty";
 
 const pdfButton = document.getElementById("pdf-button");
 
-pdfButton.addEventListener("click", async () => {
-  const pdfDoc = await PDFDocument.create();
-  const page = pdfDoc.addPage([350, 400]);
-  page.moveTo(110, 200);
-  page.drawText("Hello World!");
-  const pdfDataUri = await pdfDoc.saveAsBase64({ dataUri: true });
-  document.getElementById("pdf").src = pdfDataUri;
+import(/* webpackChunkName: "pdf-lib" */ "pdf-lib").then(({ PDFDocument }) => {
+  pdfButton.addEventListener("click", async () => {
+    const pdfDoc = await PDFDocument.create();
+    const page = pdfDoc.addPage([350, 400]);
+    page.moveTo(110, 200);
+    page.drawText("Hello World!");
+    const pdfDataUri = await pdfDoc.saveAsBase64({ dataUri: true });
+    document.getElementById("pdf").src = pdfDataUri;
+  });
 });
---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  optimization: {
+    splitChunks: {
+      // include all types of chunks
+      chunks: "all",
+    },
+  },
+};
